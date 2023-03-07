63623f51d2c124b1fcabd1b88bfdef4e0a46bd1b
charrada <61417722+charrada@users.noreply.github.com>
2023-03-07 22:42:45 +0100

    Login
    First commit

--- src/index.js
+++ src/index.js
@@ -1,45 +1,10 @@
-
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
-
-import "assets/vendor/nucleo/css/nucleo.css";
-import "assets/vendor/font-awesome/css/font-awesome.min.css";
-import "assets/scss/argon-design-system-react.scss?v1.1.0";
-
-import Index from "views/Index.js";
-import Landing from "views/examples/Landing.js";
-import Login from "views/examples/Login.js";
-import Profile from "views/examples/Profile.js";
-import Register from "views/Register.js";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render( < App / > );
 
-root.render(
-  <BrowserRouter>
-    <Switch>
-      <Route path="/Composant" exact render={(props) => <Index {...props} />} />
-      <Route
-        path="/landing-page"
-        exact
-        render={(props) => <Landing {...props} />}
-      />
-      <Route
-        path="/login-page"
-        exact
-        render={(props) => <Login {...props} />}
-      />
-      <Route
-        path="/profile-page"
-        exact
-        render={(props) => <Profile {...props} />}
-      />
-      <Route
-        path="/"
-        exact
-        render={(props) => <Register {...props} />}
-      />
-      <Redirect to="/" />
-    </Switch>
-  </BrowserRouter>
-);
+reportWebVitals();
